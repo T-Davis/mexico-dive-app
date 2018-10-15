9a4b116a5ef35b874725697b4d6fe46840938f1f
t-davis <tzendou@gmail.com>
2018-10-15 12:28:41 -0500

    First commit

---
+++ src/main/java/com/trevor/mexicodiveapp/MexicoDiveAppApplication.java
@@ -0,0 +1,12 @@
+package com.trevor.mexicodiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MexicoDiveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MexicoDiveAppApplication.class, args);
+	}
+}
